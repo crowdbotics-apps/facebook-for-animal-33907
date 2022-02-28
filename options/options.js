2f9112bc705e75a7a6d452a2dda2069c805d9f86
Crowdbotics <team@crowdbotics.com>
2022-02-28 20:35:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "facebook_for_animal_33907",
+  url: "https://facebook_for_animal_33907.botics.co",
+  api: "https://facebook_for_animal_33907.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
